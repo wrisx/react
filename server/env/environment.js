949eba3861ba9daa75e44b6c4602f47699503414
wrisx <info@wrisx.com>
2017-10-17 01:14:03 +0100

    env

--- server/env/environment.js
+++ server/env/environment.js
@@ -1,6 +1,6 @@
 const cosmosPort = 10255; // replace with your port
 const dbName = 'react';
-const key = 'ULgiOU70pEJDNoOHE77ybXYnEFsBonm5axEdgQ3y6FdbsEovohnYdtl49xrr82qBxwWuuHb9UigOySKXx9HaLw==';
+const key = 'g6uJ8ygrlQaW7asIPzDBPSKcR5rbAkGosk4QOKuqEvfrQk3APgYCVRb2T9QDCL8xlemK4Fra7CeLeIHS69ExyA==';
 
 module.exports = {
   cosmosPort,
